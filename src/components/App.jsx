a2cab5f53812218432a0cb2e9bc817ec2879dab8
Daljeet Singh Harsh <60133328+dsh35a@users.noreply.github.com>
2021-06-05 19:05:05 +0530

    Add files via upload

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Head from "./Head";
+import Bottom from "./Bottom";
+import Note from "./Note";
+
+function App() {
+  return (
+    <div>
+      <Head />
+      <Bottom />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Profile : </h1>
+      <p>
+        myself daljeet singh harsh and right now i'm doing <b>MCA</b> from RIET
+        Jaipur.Mainly I'm a Java Developer now and preparing for FullStack
+        Development with <b>MEAN Stack</b>
+      </p>
+    </div>
+  );
+}
+
+export default Note;
